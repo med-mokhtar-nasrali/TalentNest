218fa8e6235eb3746dd55ef79bfe25f54040104f
med-mokhtar-nasrali <mokhtarbackup03@gmail.com>
2024-11-14 18:33:47 +0100

    Edit freelancer profile

--- flask_app/controllers/freelancer_controller.py
+++ flask_app/controllers/freelancer_controller.py
@@ -1,8 +1,26 @@
 from flask_app import app
 from flask_app.models.user_model import User
 from flask_app.models.freelancer_model import Freelancer
-from flask_app.models.recruiter_model import Recruiter
 from flask import render_template,session,redirect,flash,request
 
+#Display Route for the edit page
+@app.route('/freelancer/edit/<int:id>')
+def edit(id):
+    if "user_id"not in session:
+        return redirect('/login')
+    freelancer = Freelancer.get_by_id({"id":id})
+    return render_template("edit_profile_freelancer.html",freelancer = freelancer)
+    
 
 
+#Action Route for the edit page
+@app.route("/freelancer/update/<int:id>",methods=["POST"])
+def update(id):
+    if Freelancer.validate(request.form):
+        data = {
+            **request.form,
+            "id":id
+        }
+        Freelancer.update(data)
+        return redirect("/home")
+    return redirect(f'/freelancer/edit/{id}')
--- flask_app/models/freelancer_model.py
+++ flask_app/models/freelancer_model.py
@@ -14,7 +14,7 @@ class Freelancer:
         self.created_at = data["created_at"]
         self.updated_at = data["updated_at"]
         self.user_id = data["user_id"]
-
+        self.posted_by = ""
 
 # this method is to insert freelancer additionl info into its table
     @classmethod
@@ -22,3 +22,44 @@ class Freelancer:
         query="insert into freelancers (phone, about_me, experience, skills, user_id) values (%(phone)s, %(about_me)s, %(experience)s, %(skills)s,%(user_id)s);"    
         return connectToMySQL( DB ).query_db(query,data)
     
+    @classmethod
+    def update(cls,data):
+        query = """
+                UPDATE freelancers SET
+                phone = %(phone)s,
+                about_me = %(about_me)s,
+                experience = %(experience)s,
+                skills = %(skills)s
+                WHERE freelancers.id = %(id)s;
+                """
+        return connectToMySQL( DB ).query_db(query,data)
+    
+
+    @classmethod
+    def get_by_id(cls,data):
+        query = """
+                SELECT * FROM freelancers
+                JOIN users ON freelancers.user_id = user_id
+                WHERE freelancers.id = %(id)s;
+                """
+        result = connectToMySQL( DB ).query_db(query,data)
+        freelancer = cls(result[0])
+        freelancer.posted_by = f'{result[0]["first_name"]}{result[0]["last_name"]}'
+        return freelancer
+
+    @staticmethod
+    def validate(data):
+        is_valid = True
+        if len(data["phone"])<8:
+            is_valid = False
+            flash("Phone number is too short","phone")
+        if len(data["about_me"])<3:
+            is_valid = False
+            flash("About Me cant be empty","about_me")
+        if len(data["experience"])<3:
+            is_valid = False
+            flash("Experience cant be empty","experience")
+        if len(data["skills"])<3:
+            is_valid = False
+            flash("Skills cant be empty","skills")
+        return is_valid
